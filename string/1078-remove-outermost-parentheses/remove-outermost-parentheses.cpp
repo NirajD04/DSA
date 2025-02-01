b5d7dc2043473047a7c99d5b10c39c71b6081e0b
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-02-01 20:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (81.13%) - LeetSync

---
+++ string/1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        //     stack<char> st;
+        //     string result = "";
+
+        //     for (int i = 0; i < s.length(); i++) {
+
+        //         if (s[i] == '(') {
+        //             if (!st.empty()) {
+        //                 result += s[i];
+        //             }
+        //             st.push(s[i]);
+
+        //         } else if (s[i] == ')') {
+        //             st.pop();
+        //             if (!st.empty()) {
+        //                 result += s[i];
+        //             }
+
+        //         }
+        //     }
+        //     return result;
+        string ans = "";
+        int depth = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                depth++;
+                if (depth > 1) {
+                    ans += s[i];
+                }
+                
+            } else if (s[i] == ')') {
+
+                if (depth > 1) {
+                    ans += s[i];
+                }
+                depth--;
+            }
+        }
+        return ans;
+    }
+};
