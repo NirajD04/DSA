34631702edf886512858ef26e1a4c8a674ac7a71
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-03-30 22:45:53 +0530

    Time: 7 ms (52.72%) | Memory: 19.2 MB (77.59%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+          vector<vector<int>> adj(numCourses);
+
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
+        
+        // Build adjacency list
+        for (auto& pre : prerequisites) {
+            int course = pre[0];
+            int prereq = pre[1];
+
+            adj[prereq].push_back(course);
+            
+            indegree[course]++;
+        }
+
+        // Push nodes with zero indegree
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int count = 0;  // Track number of processed nodes
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            count++;
+
+            for (int& v : adj[u]) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+
+        // If all courses are processed, return true
+        return count == numCourses;
+    }
+};
