8731c817153aba9a3694c312ed729dcee668757f
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-04-30 13:47:32 +0530

    Time: 249 ms (65.41%) | Memory: 10.6 MB (74.62%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -3,16 +3,26 @@ public:
     int m, n;
     vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-    bool find(vector<vector<char>>& board, int i, int j, int idx, const string &word) {
-        if (idx == word.length()) return true;
-        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != word[idx]) return false;
+    bool find(vector<vector<char>>& board, int i, int j, int idx,
+              const string& word) {
+
+        if (idx == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != word[idx]) {
+            return false;
+        }
 
         char temp = board[i][j];
+
         board[i][j] = '#'; // safer marker
 
-        for (auto &dir : direction) {
+        for (auto& dir : direction) {
+
             int new_i = i + dir[0];
             int new_j = j + dir[1];
+
             if (find(board, new_i, new_j, idx + 1, word)) {
                 return true;
             }
@@ -22,12 +32,12 @@ public:
         return false;
     }
 
-    bool exist(vector<vector<char>>& board, const string &word) {
+    bool exist(vector<vector<char>>& board, const string& word) {
         m = board.size();
         n = board[0].size();
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == word[0] && find(board, i, j, 0, word)) {
                     return true;
                 }
