cf2f548ec0c4072c91398b182f1125c5bc303e3b
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-05-14 18:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (53.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     typedef pair<int, int> P;
-
     vector<vector<int>> direction{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     int orangesRotting(vector<vector<int>>& grid) {
@@ -9,12 +8,12 @@ public:
         int n = grid[0].size();
 
         queue<P> q;
-
         int freshorange = 0;
-        int minutes = 0;
+        int minute = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+
                 if (grid[i][j] == 2) {
                     q.push({i, j});
                 } else if (grid[i][j] == 1) {
@@ -23,32 +22,34 @@ public:
             }
         }
 
+
         if (freshorange == 0)
             return 0;
 
         while (!q.empty()) {
             int N = q.size();
-
             while (N--) {
+
                 auto curr = q.front();
                 int i = curr.first;
                 int j = curr.second;
-                q.pop();
-                for (auto& dir : direction) {
-                    int new_i = i + dir[0];
-                    int new_j = j + dir[1];
 
-                    if (new_i >= 0 && new_i < m && new_j >= 0 && new_j < n &&
-                        grid[new_i][new_j] == 1) {
-                        grid[new_i][new_j] = 2; // mark as rotten
-                        q.push({new_i, new_j});
+                q.pop();
+
+                for (auto& dir : direction) {
+                    int new_x = i + dir[0];
+                    int new_y = j + dir[1];
+
+                    if (new_x >= 0 && new_x < m && new_y >= 0 && new_y < n &&
+                        grid[new_x][new_y] == 1) {
+                        grid[new_x][new_y] = 2;
+                        q.push({new_x, new_y});
                         freshorange--;
                     }
                 }
-               
             }
-             minutes++;
+            minute++;
         }
-        return freshorange == 0 ? (minutes - 1) : -1;
+        return freshorange == 0 ? (minute - 1) : -1;
     }
 };
