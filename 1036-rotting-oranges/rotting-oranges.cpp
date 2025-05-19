c75783551f453abdfc98034ca918348b56234eb7
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-05-19 16:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (32.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     typedef pair<int, int> P;
+
     vector<vector<int>> direction{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
     int orangesRotting(vector<vector<int>>& grid) {
@@ -8,6 +9,7 @@ public:
         int n = grid[0].size();
 
         queue<P> q;
+
         int freshorange = 0;
         int minute = 0;
 
@@ -15,7 +17,8 @@ public:
             for (int j = 0; j < n; j++) {
 
                 if (grid[i][j] == 2) {
-                    q.push({i, j});
+                    q.push({i, j}); // rotten oranges
+
                 } else if (grid[i][j] == 1) {
                     freshorange++;
                 }
@@ -27,23 +30,32 @@ public:
             return 0;
 
         while (!q.empty()) {
+
+             
             int N = q.size();
+
             while (N--) {
 
-                auto curr = q.front();
-                int i = curr.first;
-                int j = curr.second;
+               
+
+                int i = q.front().first;   //row
+                int j = q.front().second;  //coloum
 
                 q.pop();
 
                 for (auto& dir : direction) {
-                    int new_x = i + dir[0];
+
+                    int new_x = i + dir[0];   //mov in direction ;
                     int new_y = j + dir[1];
 
                     if (new_x >= 0 && new_x < m && new_y >= 0 && new_y < n &&
                         grid[new_x][new_y] == 1) {
+
+
                         grid[new_x][new_y] = 2;
+                        
                         q.push({new_x, new_y});
+
                         freshorange--;
                     }
                 }
