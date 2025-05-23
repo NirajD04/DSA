8a3dbdfac29ffeae621f6a93c851b0bbf0c236a0
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-05-23 12:33:21 +0530

    Time: 17 ms (20.88%) | Memory: 19.1 MB (53.73%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,45 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root==nullptr) return {};
+        if (root == NULL) return NULL;
 
         queue<Node*> q;
         q.push(root);
-        
-        while(!q.empty()){
-            int n = q.size();
+        q.push(NULL); // Marker for end of level
 
-            for(int i=0;i<n;i++){
-                Node* t = q.front();
-                q.pop();
+        while (!q.empty()) {
+            Node* front = q.front();
+            q.pop();
 
-                if(i!=n-1){
-                    t->next=q.front();
+            if (front != NULL) {
+                // Connect current node to the next node in queue (could be NULL)
+                front->next = q.front();
+
+                // Push children to queue
+                if (front->left != NULL) q.push(front->left);
+                if (front->right != NULL) q.push(front->right);
+            } else {
+                // End of level reached
+                if (!q.empty()) {
+                    q.push(NULL); // Add marker for next level
                 }
-
-                if(t->left) q.push(t->left);
-
-                if(t->right) q.push(t->right);
             }
         }
+
         return root;
     }
 };
