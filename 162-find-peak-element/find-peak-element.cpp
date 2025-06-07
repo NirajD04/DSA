86d515cedbd33a8645a68bb5f2e552d5dbb9c057
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-06-07 16:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (31.69%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,7 +5,7 @@ public:
         int right=nums.size()-1;
 
         while(left<right){
-            int mid=left+ (right-left) /2;
+            int mid=left + (right-left)/2;
             
             if(nums[mid]>nums[mid+1]){
                 right=mid;
