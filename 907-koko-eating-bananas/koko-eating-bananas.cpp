4447407f9ddc49f0b08f2398a84624a0bf8fff26
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-06-07 16:28:11 +0530

    Time: 12 ms (34.16%) | Memory: 22.8 MB (73.86%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    bool caneatall(vector<int>& piles, int mid, int h) {
-        long long int actualhours = 0;
-        for (int x : piles) {
-            actualhours += x  / mid;
-            if(x%mid!=0){
-                actualhours++;
-            }  // Efficient ceil(x / mid)
-        }
-        return actualhours <= h;
-    }
-   
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1 ;
-        int r = *max_element(piles.begin(), piles.end());
+    bool caneat(vector<int>&piles, int h, int mid){
+        int actualhours=0;
+        for(int x: piles){
+            actualhours+= x / mid;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (caneatall(piles, mid, h)) {
-                r = mid-1 ;  // Fix: Reduce search space correctly
-            } else {
-                l = mid + 1;
+            if(x%mid!= 0){
+                actualhours++;
+            }
+        }
+        return actualhours<= h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+
+        int l=1;
+        int r= *max_element(piles.begin(), piles.end());
+
+        while(l<r){
+            int mid= l+(r-l)/2;
+
+            if(caneat(piles, h, mid)){
+                r=mid; // shrink kar , mid ke peche ke element check kar;;
+            }
+
+            else{
+                l=mid+1;
             }
         }
         return l;
