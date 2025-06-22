e3e9c446b6017d1f3015822b3f2958620ac96b1b
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-06-22 15:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (63.17%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    bool dfs(int u, int v, unordered_map<int, vector<int>>& adj,
-             vector<bool>& visited) {
-
-        visited[u] = true;
-
-        if (u == v) {   // ngbr  ke through  u-> ngbr->  v  i.e u== v connected
-            return true;
-        }
-
-        for (auto ngbr : adj[u]) {
-            if (visited[ngbr])
-                continue;
-            if (dfs(ngbr, v, adj, visited)) {
-                return true;
-            }
-        }
-        return false;
+    vector<int> parent;
+    vector<int> rank;
+    
+    int find(int x) {
+        if(parent[x] == x)
+            return x;
+        
+        return parent[x] = find(parent[x]); // Path compression
     }
+    
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+ 
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        unordered_map<int, vector<int>> adj;
-        
+        parent.resize(n + 1);
+        rank.resize(n + 1);
 
-        for (auto edge : edges) {
+        for(int i = 1; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+
+        for(auto& edge : edges) {
             int u = edge[0];
             int v = edge[1];
-            
-            vector<bool> visited(n, false);
-            
-            if (adj.find(u) != adj.end() && adj.find(v) != adj.end() &&
-                dfs(u, v, adj, visited)) {
+
+            if(find(u) == find(v)) {
                 return edge;
             }
 
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            Union(u, v);
         }
         return {};
     }
