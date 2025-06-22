31f50a7abe3d4ea9ae19aac934b5328bddfc415f
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-06-22 17:05:45 +0530

    Time: 3 ms (72.08%) | Memory: 16 MB (26.56%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<string> ans;
-    void solve(int left, int right, string temp) {
-        if (left == 0 && right == 0) {  // zero hone pe ans 
-            ans.push_back(temp);
-            return;
-        }
-
-        if (left > 0) {   // left me (  push kiya baki recursion
-            solve(left - 1, right, temp + "(");
-        }
-        if (right > left) {   // same right me
-            solve(left, right - 1, temp + ")");
-        } 
+vector<string> ans;
+void solve(int left, int right, string curr){
+    if(left==0 && right==0){
+        ans.push_back(curr);
+        return;
     }
+    if(left>0){
+        solve(left-1, right, curr+"(");
+    }
+    if(right>left){
+        solve(left, right-1, curr+")");
+    }
+}
     vector<string> generateParenthesis(int n) {
-        string temp = "";
-        solve(n, n, temp);
+        string temp="";
+        solve(n,n,temp);
         return ans;
     }
 };
