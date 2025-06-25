75473d0d31e060b833ad737dfb96da34d9668491
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-06-25 17:12:25 +0530

    Time: 8 ms (60.70%) | Memory: 22.9 MB (73.55%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    bool caneat(vector<int>&piles, int h, int mid){
-        int actualhours=0;
-        for(int x: piles){
-            actualhours+= x / mid;
+    bool caneat(vector<int>& piles, int h, int mid) {
+        int actualhours = 0;
+        for (int x : piles) {
+            actualhours += x / mid;
 
-            if(x%mid!= 0){
+            if (x % mid != 0) {
                 actualhours++;
             }
         }
-        return actualhours<= h;
+        return actualhours <= h;
+        
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        
 
-        int l=1;
-        int r= *max_element(piles.begin(), piles.end());
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
 
-        while(l<r){
-            int mid= l+(r-l)/2;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
 
-            if(caneat(piles, h, mid)){
-                r=mid; // shrink kar , mid ke peche ke element check kar;;
+            if (caneat(piles, h, mid)) {
+                r = mid; // shrink kar , mid ke peche ke element check kar;;
             }
 
-            else{
-                l=mid+1;
+            else {
+                l = mid + 1;
             }
         }
         return l;
