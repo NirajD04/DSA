46cb2950b5c0290a8d8517642523a4486bab483f
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-07-31 17:29:10 +0530

    Time: 41 ms (74.34%) | Memory: 94 MB (40.39%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -4,17 +4,22 @@ public:
         int n = intervals.size();
 
         sort(intervals.begin(), intervals.end(),
-             [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; });
+             [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; }); // end time pe sort kiya
 
         int end = intervals[0][1];
+
         int count = 1;
+
         for (int i = 1; i < n; i++) {
+
             if (intervals[i][0] >= end) {
-                count++;
-                end = intervals[i][1];
+
+                count++;           // non overlapping , wale count kiya, jo ache hai , 
+                
+                end = intervals[i][1];           
             }
         }
 
-        return intervals.size() - count;
+        return intervals.size() - count;    // jo bache hoge unko remove;
     }
 };
