720c4aeb5f6462bb6f3f945d9cf19974f5c98597
Niraj Dhanore <nirajdhanore04@gmail.com>
2025-09-03 20:40:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.03%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int l=1;
+        int h=num;
+        while(l <= h){
+        
+            long long mid= l+ (h-l)/2;
+
+            if(mid * mid == num){
+                return true;
+            }
+            if(mid * mid > num){
+                h=mid-1;
+            }
+            else{
+                l= mid+1;
+            }
+        }
+
+        return false;
+    }
+};
