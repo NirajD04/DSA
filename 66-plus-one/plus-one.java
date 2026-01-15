56fae7f34f857b4a94573ee950b854a7b513a8e8
Niraj Dhanore <nirajdhanore04@gmail.com>
2026-01-15 22:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (50.62%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        int i = n - 1;
 
-        while (i >= 0) {
-            if (digits[i] < 9) {
-                digits[i] += 1;
-                return digits; // No carry needed
-            }
-            digits[i] = 0; // Carry over
-            i--;
+     int n= digits.length;
+     int i=n-1;
+
+     while(i>=0){
+
+        if(digits[i]< 9){
+           digits[i]=digits[i]+1;
+           return digits;
         }
+        digits[i]=0;
+        i--;
 
-        // If we reach here, all digits were 9 (like 999 → 1000)
-        int[] result = new int[n + 1];
-        result[0] = 1; // First digit is 1, rest default to 0
-        return result;
+
+     }   
+
+     int [] arr= new int[n+1];
+     arr[0]=1;
+     return arr;
     }
 }
